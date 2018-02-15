26d0fde36c66a7589a085644476925db382f6881
Andrew Bloom <blooma116@gmail.com>
2018-02-14 17:18:00 -0800

    using big integer library to handle large significant digits

--- power-digit-sum/lib/solution.js
+++ power-digit-sum/lib/solution.js
@@ -1,20 +1,19 @@
 'use strict';
 
+let bigInt = require('big-integer');
+
 // 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
 // What is the sum of the digits of the number 2^1000 ?
 
 const sumOfPowerDigits = (base, exponent) => {
-  const resultOfExponent = Math.pow(base, exponent);
-  console.log(resultOfExponent);
+  const resultOfExponent = bigInt(base).pow(exponent);
   const numberAsString = resultOfExponent.toString();
-  console.log(numberAsString);
   const arrayOfDigits = numberAsString.split('');
-  console.log(arrayOfDigits);
   return arrayOfDigits.reduce((acc, val) => {
     const valueAsNumber = Number(val);
     return acc + valueAsNumber;
   }, 0);
 };
 
-console.log(sumOfPowerDigits(2, 10));
+console.log(sumOfPowerDigits(2, 1000));
