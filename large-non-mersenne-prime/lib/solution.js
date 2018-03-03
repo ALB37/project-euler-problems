bfcb363dd01ff40aaca31a76c3a24168baf6b97a
Andrew Bloom <blooma116@gmail.com>
2018-03-02 22:21:42 -0800

    solved large non-mersenne prime problem

---
+++ large-non-mersenne-prime/lib/solution.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// The first known prime found to exceed one million digits was discovered in 1999, and is a Mersenne prime of the form 26972593−1; it contains exactly 2, 098, 960 digits.Subsequently other Mersenne primes, of the form 2p−1, have been found which contain more digits.
+
+//   However, in 2004 there was found a massive non - Mersenne prime which contains 2, 357, 207 digits: 28433×2^7830457 + 1.
+
+// Find the last ten digits of this prime number.
+
+const bigInt = require('big-integer');
+
+console.log(bigInt(28433).multiply(bigInt(2).pow(bigInt(7830457))).add(bigInt(1)).toString().slice(-10));
