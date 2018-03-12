d116fc45da637bd71f42438fc915f3a629ddbfbe
Andrew Bloom <blooma116@gmail.com>
2018-03-11 21:21:51 -0700

    working on optimization, runs faster, but still could be further optimized

--- counting-summations/lib/solution.js
+++ counting-summations/lib/solution.js
@@ -10,3 +10,34 @@
 // 1 + 1 + 1 + 1 + 1
 
 // How many different ways can one hundred be written as a sum of at least two positive integers ?
+
+// start with 100 1s, combine 2, 3, 4 ... 99. At each step, start with that number and the remainder of 1s and start over.
+
+const sumCombinations = sum => {
+  const arrayOfOnes = [];
+  for (let num = 0; num < sum; num++){
+    arrayOfOnes.push(1);
+  }
+  const uniqueCombinations = new Set();
+  const _helper = array => {
+    array.sort((a, b) => a - b);
+    if (uniqueCombinations.has(array.toString())){
+      return;
+    } else {
+      uniqueCombinations.add(array.toString());
+    }
+    if (array.length > 2){
+      const arrayCopy = [...array];
+      const valueToAdd = arrayCopy.shift();
+      for (let i = 0; i < arrayCopy.length; i++){
+        const innerArrayCopy = [...arrayCopy];
+        innerArrayCopy[i] += valueToAdd;
+        _helper(innerArrayCopy);
+      }
+    }
+  };
+  _helper(arrayOfOnes);
+  return uniqueCombinations;
+};
+
+console.log(sumCombinations(100).size);
